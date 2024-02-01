649055bff66ac83f34f1fdbd06d7444ad66f21ab
Gost <bmood1@gmail.com>
2024-01-31 23:00:52 -0600

    init commit for gaming group project.

---
+++ src/main/java/com/gost/gaminggroup/gaminggroupapp/GamingGroupAppApplication.java
@@ -0,0 +1,13 @@
+package com.gost.gaminggroup.gaminggroupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingGroupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingGroupAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gost/gaminggroup/gaminggroupapp/GamingGroupAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gost.gaminggroup.gaminggroupapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingGroupAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
