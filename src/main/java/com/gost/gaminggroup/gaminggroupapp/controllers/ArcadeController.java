da9da57a5cde434f9188d8443b602399c0b7a216
Gost <bmood1@gmail.com>
2024-02-07 23:37:07 -0600

    Form partially implemented.

--- src/main/java/com/gost/gaminggroup/gaminggroupapp/controllers/ArcadeController.java
+++ src/main/java/com/gost/gaminggroup/gaminggroupapp/controllers/ArcadeController.java
@@ -1,11 +1,14 @@
 package com.gost.gaminggroup.gaminggroupapp.controllers;
 
 import com.gost.gaminggroup.gaminggroupapp.dto.ArcadeDto;
+import com.gost.gaminggroup.gaminggroupapp.models.Arcade;
 import com.gost.gaminggroup.gaminggroupapp.services.ArcadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,5 +26,18 @@ public class ArcadeController {
         List<ArcadeDto> arcades = arcadeService.findAllArcades();
         model.addAttribute("arcades", arcades);
         return "arcades-list";
+    }
+
+    @GetMapping("/arcades/new")
+    public String createArcadeForm(Model model){
+        Arcade arcade = new Arcade();
+        model.addAttribute("arcade", arcade);
+        return "arcades-create";
+    }
+
+    @PostMapping("/arcades/new")
+    public String saveArcade(@ModelAttribute("arcade") Arcade arcade) {
+        arcadeService.save();
+        return "redirect:/clubs";
     }
 }
