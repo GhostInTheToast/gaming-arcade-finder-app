f84c5ef83fbb1d6035a845cbac9f51da0f66d87e
Gost <bmood1@gmail.com>
2024-02-12 01:59:33 -0600

    layout

--- src/main/java/com/gost/gaminggroup/gaminggroupapp/repository/ArcadeRepository.java
+++ src/main/java/com/gost/gaminggroup/gaminggroupapp/repository/ArcadeRepository.java
@@ -2,7 +2,6 @@ package com.gost.gaminggroup.gaminggroupapp.repository;
 
 import com.gost.gaminggroup.gaminggroupapp.models.Arcade;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface ArcadeRepository extends JpaRepository<Arcade, Integer> {
